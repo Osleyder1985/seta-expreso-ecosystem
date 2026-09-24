3bf3f244d470573d84b8707f3a158149b013414d
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-24 15:42:25 -0400

    fix(poc): correct jest.config.js CI compatibility

--- poc/backend/nestjs/jest.config.js
+++ poc/backend/nestjs/jest.config.js
@@ -1,1 +1,7 @@
-module.exports={moduleFileExtensions:['js','json','ts'],rootDir:'.',testRegex:'.*\\.spec\\.ts$',transform:{'^.+\\.(t|j)s$':'ts-jest'},testEnvironment:'node'};
+module.exports = {
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.e2e-spec\\.ts$',
+  transform: { '^.+\\.(t|j)s$': 'ts-jest' },
+  testEnvironment: 'node',
+};
