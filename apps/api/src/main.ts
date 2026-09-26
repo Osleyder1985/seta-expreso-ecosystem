cc6a493c3c4231cb0270920e508f952c2934e53d
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-26 15:39:54 -0400

    security(api): enable NestJS security headers baseline

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
+  app.useSecurityHeaders();
+
   app.setGlobalPrefix('api');
   app.enableShutdownHooks();
 
