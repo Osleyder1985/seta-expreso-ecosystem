e299c746e22ec0b768aa0cfe2d922d5b5add3909
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-26 15:42:22 -0400

    security(api): enforce global request throttling

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,11 +5,25 @@ import { HealthModule } from './modules/health/health.module';
 
 @Module({
   imports: [
-    ThrottlerModule.forRoot({
-      throttlers: [{ name: 'default', ttl: 60000, limit: 100 }],
-    }),
+    ThrottlerModule.forRoot([
+      {
+        name: 'short',
+        ttl: 1000,
+        limit: 20,
+      },
+      {
+        name: 'long',
+        ttl: 60000,
+        limit: 300,
+      },
+    ]),
     HealthModule,
   ],
-  providers: [{ provide: APP_GUARD, useClass: ThrottlerGuard }],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
