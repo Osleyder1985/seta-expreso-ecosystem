5a4c06b28f703ef6b15c472de183e997fd4246c4
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-26 17:08:29 -0400

    feat(data): register database module #179

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { HealthModule } from './modules/health/health.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { DatabaseModule } from './modules/database/database.module';
 
 @Module({
-  imports: [AuthModule, HealthModule],
+  imports: [AuthModule, DatabaseModule, HealthModule],
 })
 export class AppModule {}
