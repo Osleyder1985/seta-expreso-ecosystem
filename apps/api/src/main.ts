4e1466f595fcff9832eaa9220a3e872ed265f49d
Osleyder1985 <osleyder.gonzalez1985@gmail.com>
2026-09-26 15:44:36 -0400

    security(api): register safe HTTP error filter

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpErrorFilter } from './common/http/http-error.filter';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,7 @@ async function bootstrap(): Promise<void> {
 
   app.setGlobalPrefix('api');
   app.enableShutdownHooks();
+  app.useGlobalFilters(new HttpErrorFilter());
 
   app.useGlobalPipes(
     new ValidationPipe({
